f930e198250cdaac088d7ba7700c92c658a75668
David <kantorda@umich.edu>
2018-03-03 18:22:07 -0500

    using *util packages to find and run execs

--- alertMe.py
+++ alertMe.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os, sys, smtplib, concurrent.futures
+import os, sys, smtplib, concurrent.futures, shutil, psutil
 from email.mime.text import MIMEText
 
 def sendNotification(msgBody):
@@ -7,5 +7,30 @@ def sendNotification(msgBody):
     # will need to prompt user for credentials
     print("alert: {}".format(msgBody))
 
+def getPath():
+    execPath = ""
+    try:
+        execPath = shutil.which(sys.argv[1])
+    except:
+        print("invalid number of arguments")
+    if execPath is None:
+        execPath = os.getcwd() + "/" + sys.argv[1]
+    return execPath
+
+def runCmd(execPath):
+    argList = [execPath]
+    for arg in sys.argv[2:]:
+        argList.append(arg)
+    try:
+        process = psutil.Popen(argList)
+    except:
+        print(sys.exc_info())
+        return None
+    return process
+
 if __name__ == "__main__":
-    sendNotification("hello world")
+    execPath = getPath()
+    process = runCmd(execPath)
+    if process is not None:
+        print(process.name)
+        print(process.pid)
